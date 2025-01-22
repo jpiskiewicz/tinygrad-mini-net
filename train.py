51c831a03a2b5e4b104a8b127d317db0e81ca9f8
Jakub Piskiewicz <jakub@piskiewicz.org>
2025-01-22 20:58:08 +0100

    WIP: Total loss

--- train.py
+++ train.py
@@ -15,13 +15,12 @@ def jaccard_loss(prediction: Tensor, target: Tensor) -> Tensor:
   return Tensor.ones().sub(prediction.mul(target).sum().div(prediction.sum().add(target.sum())))
 
 
+def total_loss(prediction: Tensor, target: Tensor) -> Tensor:
+  return dice_loss(prediction, target).add(jaccard_loss(prediction, target)).add(prediction.binary_crossentropy(target))
+
+
 if __name__ == "__main__":
   net = Net()
   dataset = Dataset("../UNet/tinygrad-UNet/dataset.safetensors")
   dataset.remove_object_ids()
-  print(dice_loss(dataset.images[0], dataset.masks[0]).numpy())
-  print(jaccard_loss(dataset.images[0], dataset.masks[0]).numpy())
-  print(net(dataset.images[Tensor.zeros(1, dtype="int")]).binary_crossentropy(dataset.masks[Tensor.zeros(1, dtype="int")]).numpy())
-
-  # I don't want to complicate the network by introducing another layer with
-  # object IDs so we should remove this layer from the masks before feeding them to the network.
+  print(total_loss(net(dataset.images[Tensor.zeros(1, dtype="int")]), dataset.masks[Tensor.zeros(1, dtype="int")]).numpy())
